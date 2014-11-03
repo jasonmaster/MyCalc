5bd89fd5bfaefc09b34d4b95ed7150ad5ad5afec
Jason Sweeney <admin@jay-designs.com>
2014-11-03 06:38:49 -0500

    Change out the math works
    Added new features, and changes how to math calculates

--- app/src/main/java/app/jaydesigns/com/mycalc/MainActivity.java
+++ app/src/main/java/app/jaydesigns/com/mycalc/MainActivity.java
@@ -15,7 +15,7 @@ import android.util.FloatMath;
 public class MainActivity extends Activity implements View.OnClickListener {
 
 
-    Button bttn_plus, bttn_min, bttn_x, bttn_divide, bttn_eq, bttn_clear, bttn_ce, bttn_close, bttn_sqr;
+    Button bttn_plus, bttn_min, bttn_x, bttn_divide, bttn_eq, bttn_clear, bttn_ce, bttn_close, bttn_sq,bttn_exp, bttn_log;
     EditText textbox_input;
     TextView view,audit;
 
@@ -23,6 +23,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
     int op1;
     int op2;
     String optr;
+    String optr2;
+
 
 
     @Override
@@ -40,7 +42,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
         bttn_clear = (Button) findViewById(R.id.bttn_clear);
         bttn_ce = (Button) findViewById(R.id.bttn_ce);
         bttn_close = (Button) findViewById(R.id.bttn_close);
-        //bttn_sqr = (Button) findViewById(R.id.bttn_sqr);
+        bttn_sq = (Button) findViewById(R.id.bttn_sq);
+        bttn_exp = (Button) findViewById(R.id.bttn_exp);
+        //bttn_log = (Button) findViewById(R.id.bttn_log);
 
         textbox_input = (EditText) findViewById(R.id.textbox_input);
 
@@ -57,7 +61,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
             bttn_clear.setOnClickListener(this);
             bttn_ce.setOnClickListener(this);
             bttn_close.setOnClickListener(this);
-            //bttn_sqr.setOnClickListener(this);
+            bttn_sq.setOnClickListener(this);
+            bttn_exp.setOnClickListener(this);
 
         } catch(Exception e){
 
@@ -116,9 +121,92 @@ public class MainActivity extends Activity implements View.OnClickListener {
             view.setText(Integer.toString(op1));
             textbox_input.setText("");
         }
+        else if (optr.equals("^")) {
+            op2 = Integer.parseInt(textbox_input.getText().toString());
+            audit.setText(audit.getText() + optr + Integer.toString(op2) );
+            op1 = (int)(my_pow(op1,op2));
+            view.setText(Integer.toString(op1));
+            textbox_input.setText("");
+        }
+        else if (optr.equals("sqrt")) {
+            op2 = Integer.parseInt(textbox_input.getText().toString());
+            audit.setText(audit.getText() + optr + Integer.toString(op2) );
+            op1 = (int)(Math.sqrt(op2));
+            view.setText(Integer.toString(op1));
+            textbox_input.setText("");
+        }
     }
 
+    public static double my_pow(double base, int exponent) {
+        if(base == 0.0) return 0.0;
+        if(exponent == 0) return 1.0;
 
+        if(exponent < 0) {
+            base = 1 / base;
+            exponent = -exponent;
+        }
+
+        double result = 1.0;
+        for(int i = 0; i < exponent; i++) {
+            result *= base;
+        }
+
+        return result;
+    }
+
+    public void math(){
+        if(textbox_input.getText().toString() != null){
+            if (op1 == 0) {
+               // int op1_a;
+                op1 = Integer.parseInt(textbox_input.getText().toString());
+                //op1_a = Integer.parseInt(textbox_input.getText().toString());
+                audit.setText(String.valueOf(op1));
+                if(optr == "sqrt"){
+                    audit.setText("SQRT(" + String.valueOf(op1) + ")");
+                    op1 = (int)(Math.sqrt(op1));
+                    optr = "";
+                }
+                view.setText(String.valueOf(op1));
+
+                textbox_input.setText("");
+            } else if (op2 != 0) {
+                op2 = 0;
+                view.setText(textbox_input.getText().toString());
+
+                textbox_input.setText("");
+            } else {
+                op2 = Integer.parseInt(textbox_input.getText().toString());
+                textbox_input.setText("");
+                if(optr2 == "x^2"){
+                    audit.setText(audit.getText() + optr + "(" + Integer.toString(op2) + ")" + optr2 );
+                }else{
+                    audit.setText(audit.getText() + optr2 + Integer.toString(op1) );
+                }
+
+
+                if (optr2 == "+"){
+                    op1 = op1 + op2;
+                }else if (optr2 == "-"){
+                    op1 = op1 - op2;
+                }else if (optr2 == "*"){
+                    op1 = op1 * op2;
+                }else if (optr2 == "/"){
+                    op1 = op1 / op2;
+                }else if (optr2 == "^"){
+                    op1 = (int)(my_pow(op1,op2));
+                }else if(optr2 == "sqrt"){
+                    op1 = (int)(Math.sqrt(op2));
+                }
+                //op1 = op1 + op2;
+                op2=0;
+                view.setText(Integer.toString(op1));
+            }
+            optr2 = optr;
+
+        }
+
+
+    }
 
     @Override
     public void onClick(View arg0){
@@ -140,6 +228,9 @@ public class MainActivity extends Activity implements View.OnClickListener {
                 bttn_x.setEnabled(true);
                 bttn_divide.setEnabled(true);
                 bttn_eq.setEnabled(true);
+                bttn_exp.setEnabled(true);
+                bttn_sq.setEnabled(true);
+
             break;
             case R.id.bttn_ce:
                 textbox_input.setText("");
@@ -149,7 +240,7 @@ public class MainActivity extends Activity implements View.OnClickListener {
 
             case R.id.bttn_plus:
                 optr = "+";
-                if (op1 == 0) {
+                /*if (op1 == 0) {
                     op1 = Integer.parseInt(textbox_input.getText().toString());
                     view.setText(textbox_input.getText().toString());
                     audit.setText(audit.getText() + Integer.toString(op1) );
@@ -163,17 +254,32 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     op2 = Integer.parseInt(textbox_input.getText().toString());
                     textbox_input.setText("");
                     audit.setText(audit.getText() + optr + Integer.toString(op2) );
+
                     op1 = op1 + op2;
                     op2=0;
                     view.setText(Integer.toString(op1));
-                }
+                }*/
+
+                math();
+
             break;
+
+            case R.id.bttn_exp:
+                optr = "^";
+                math();
+
+                break;
+            case R.id.bttn_sq:
+                optr = "sqrt";
+                math();
+
+                break;
 
 
 
             case R.id.bttn_min:
                 optr = "-";
-                if (op1 == 0) {
+                /*if (op1 == 0) {
                     op1 = Integer.parseInt(textbox_input.getText().toString());
                     view.setText(textbox_input.getText().toString());
                     audit.setText(audit.getText() + Integer.toString(op1) );
@@ -190,12 +296,13 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     op1 = op1 - op2;
                     op2=0;
                     view.setText(Integer.toString(op1));
-                }
+                }*/
+                math();
                 break;
 
             case R.id.bttn_x:
                 optr = "*";
-                if (op1 == 0) {
+                /*if (op1 == 0) {
                     op1 = Integer.parseInt(textbox_input.getText().toString());
                     view.setText(textbox_input.getText().toString());
                     audit.setText(audit.getText() + Integer.toString(op1) );
@@ -212,13 +319,14 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     op1 = op1 * op2;
                     op2=0;
                     view.setText(Integer.toString(op1));
-                }
+                }*/
+                math();
                 break;
 
 
             case R.id.bttn_divide:
                 optr = "/";
-                if (op1 == 0) {
+                /*if (op1 == 0) {
                     op1 = Integer.parseInt(textbox_input.getText().toString());
                     view.setText(textbox_input.getText().toString());
                     audit.setText(audit.getText() + Integer.toString(op1) );
@@ -235,7 +343,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
                     op1 = op1 / op2;
                     op2=0;
                     view.setText(Integer.toString(op1));
-                }
+                }*/
+                math();
                 break;
 
             case R.id.bttn_eq:
@@ -256,6 +365,8 @@ public class MainActivity extends Activity implements View.OnClickListener {
                         bttn_x.setEnabled(false);
                         bttn_divide.setEnabled(false);
                         bttn_eq.setEnabled(false);
+                        bttn_exp.setEnabled(false);
+                        bttn_sq.setEnabled(false);
 
                         operation();
                     }
